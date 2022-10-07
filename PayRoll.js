db559c30f12a7f95a0d1bbb7667487681336ed90
Sandeep singh <sandy180388@gmail.com>
2022-10-07 08:25:45 +0530

    added To get daily working hrs

--- PayRoll.js
+++ PayRoll.js
@@ -12,16 +12,16 @@ else
     console.log("Employee is Present");
 }
 
-//UC2 calculate daily emp wage based on part time and full time
+//UC-3 function that returns the emp hrs based on full time and part time
 console.log("***UC2 calculate daily emp wage based on part time and full time and absent***")
+console.log("***Refactored UC-2 for UC-3 Function that returns the emp hrs based on full time and part time***");
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
 const WAGE_PER_HR=20;
-let empHrs=0;
-var empWage = 0;
 empCheck=Math.floor(Math.random()*10)%3;
+function getWorkinghours(empCheck){
 switch(empCheck)
 {
     case IS_PART_TIME:
@@ -36,6 +36,11 @@ switch(empCheck)
         console.log("Absent")
         empHrs=0;
         break;
+   
 }
-empWage=empHrs*WAGE_PER_HR;
-console.log("Emp Wage : "+empWage);
+}
+let empHrs1=0;
+let empWage1=0;
+empHrs1=getWorkinghours(empCheck);
+empWage1=empHrs*WAGE_PER_HR;
+console.log("Emp Wage : "+empWage1);
