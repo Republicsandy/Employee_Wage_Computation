444e0d33a2c162b9053de0c898eb74ad84f0d4c7
Sandeep singh <sandy180388@gmail.com>
2022-10-07 08:22:39 +0530

    Added calculate daily emp wage in UC_2

--- PayRoll.js
+++ PayRoll.js
@@ -11,3 +11,31 @@ else
 {
     console.log("Employee is Present");
 }
+
+//UC2 calculate daily emp wage based on part time and full time
+console.log("***UC2 calculate daily emp wage based on part time and full time and absent***")
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const WAGE_PER_HR=20;
+let empHrs=0;
+var empWage = 0;
+empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        console.log("Part Time")
+        empHrs=PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        console.log("Full Time")
+        empHrs=FULL_TIME_HRS;
+         break;
+    default:
+        console.log("Absent")
+        empHrs=0;
+        break;
+}
+empWage=empHrs*WAGE_PER_HR;
+console.log("Emp Wage : "+empWage);
